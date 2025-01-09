c3f10cec86552175b0c5232a86d046c88537f99c
KingsTokas1 <ndenojuotochukwukingsley@gmail.com>
2025-01-10 06:52:57 +0800

    Add files via upload

---
+++ about.js
@@ -0,0 +1,10 @@
+function changeContent() {
+    let data = document.getElementById("data");
+    data.innerText = "As a comapny we are here to safe and secure At EasyTrust Courier Services, ensuring the safety of our customers, employees and our communities is our priority. your packages any day any time so you are very safe with more company As a company, we have a solid safety performance history and will continue to be a leader in the area of safety and compliance";
+}
+
+
+function changeContent2() {
+    let data2 = document.getElementById("data2");
+    data2.innerText = "As a comapny we are here to safe and secure At EasyTrust Courier Services, ensuring the safety of our customers, employees and our communities is our priority. your packages any day any time so you are very safe with more company As a company, we have a solid safety performance history and will continue to be a leader in the area of safety and compliance MAIN DATA TO SAFE ALL ";
+}
